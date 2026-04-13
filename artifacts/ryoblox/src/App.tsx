a0f01602ce66346097be1dfedf97a8b8282926a7
Neporrex <neporrex@gmail.com>
2026-04-13 10:07:06 +0200

    Update App.tsx

--- artifacts/ryoblox/src/App.tsx
+++ artifacts/ryoblox/src/App.tsx
@@ -7,6 +7,7 @@ import Home from "@/pages/Home";
 import Privacy from "@/pages/Privacy";
 import Tos from "@/pages/Tos";
 import Instructions from "@/pages/Setup";
+import Dashboard from "@/pages/Dashboard";
 import NotFound from "@/pages/not-found";
 
 const queryClient = new QueryClient();
@@ -18,6 +19,7 @@ function Router() {
       <Route path="/setup" component={Instructions} />
       <Route path="/privacy" component={Privacy} />
       <Route path="/tos" component={Tos} />
+      <Route path="/dashboard" component={Dashboard} />
       <Route component={NotFound} />
     </Switch>
   );
